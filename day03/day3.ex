dc62b002618ce5970f989145354662048551d0f3
jdl <confusionary@gmail.com>
2022-12-06 21:30:04 -0500

    day3 part1

---
+++ day03/day3.ex
@@ -0,0 +1,37 @@
+defmodule Day3 do
+  def common([a, b]) do
+    MapSet.intersection(a, b)
+  end
+
+  def split_ruck(ruck) do
+    String.split_at(ruck, trunc(String.length(ruck) / 2))
+  end
+
+  def priority(<<char::utf8>>) do
+    cond do
+      ?a <= char and char <= ?z -> char - 96
+      ?A <= char and char <= ?Z -> char - 38
+      true -> 0
+    end
+  end
+
+  def go() do
+    {:ok, input} = File.read("../day03/input.txt")
+
+    input
+    |> String.split("\n")
+    |> Enum.map(&split_ruck/1)
+    |> Enum.map(fn x -> Tuple.to_list(x) end)
+    |> Enum.map(fn [a, b] ->
+      [MapSet.new(String.to_charlist(a)), MapSet.new(String.to_charlist(b))]
+    end)
+    |> Enum.map(&common/1)
+    |> Enum.map(&Enum.to_list/1)
+    |> Enum.map(&List.first/1)
+    |> Enum.filter(fn x -> !is_nil(x) end)
+    |> Kernel.to_string()
+    |> String.split("", trim: true)
+    |> Enum.map(&priority/1)
+    |> Enum.sum()
+  end
+end
