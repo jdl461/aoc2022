6acafa5002ed1d220f6137dc3385095b95f0ab15
jdl <confusionary@gmail.com>
2022-12-07 21:02:39 -0500

    day4 part2

--- day04/day4.ex
+++ day04/day4.ex
@@ -3,8 +3,15 @@ defmodule Day4 do
     String.split(s, "-", trim: true) |> Enum.map(&String.to_integer/1)
   end
 
-  def overlaps([[a, b], [c, d]]) do
+  def contain([[a, b], [c, d]]) do
     (a <= c && b >= d) || (c <= a && d >= b)
+  end
+
+  def overlap([[a, b], [c, d]]) do
+    MapSet.intersection(
+      MapSet.new(a..b),
+      MapSet.new(c..d)
+    )
   end
 
   def part1() do
@@ -16,8 +23,23 @@ defmodule Day4 do
     |> Enum.map(fn [a, b] ->
       [to_range(a), to_range(b)]
     end)
-    |> Enum.map(&overlaps/1)
-    |> Enum.filter(fn x -> x end)
+    |> Enum.map(&contain/1)
+    |> Enum.filter(&Function.identity/1)
+    |> Enum.count()
+  end
+
+  def part2() do
+    {:ok, input} = File.read("input.txt")
+
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x -> String.split(x, ",", trim: true) end)
+    |> Enum.map(fn [a, b] ->
+      [to_range(a), to_range(b)]
+    end)
+    |> Enum.map(&overlap/1)
+    |> Enum.map(&Enum.to_list/1)
+    |> Enum.filter(fn x -> Enum.count(x) > 0 end)
     |> Enum.count()
   end
 end
