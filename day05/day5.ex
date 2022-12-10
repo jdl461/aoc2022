3d0ecb5d578700683e561b6c05d7e102e0538caa
jdl <confusionary@gmail.com>
2022-12-10 16:54:28 -0500

    day5 part2

--- day05/day5.ex
+++ day05/day5.ex
@@ -1,5 +1,5 @@
 defmodule Day5 do
-  def move(diagram, count, from, to) do
+  def move(diagram, count, from, to, func) do
     f = diagram[from]
     t = diagram[to]
 
@@ -7,7 +7,7 @@ defmodule Day5 do
 
     diagram
     |> Map.put(from, keeping)
-    |> Map.put(to, Enum.reverse(moving) ++ t)
+    |> Map.put(to, func.(moving) ++ t)
   end
 
   def part1() do
@@ -38,7 +38,40 @@ defmodule Day5 do
     |> Enum.reduce(
       diagram,
       fn [[count, from, to]], acc ->
-        move(acc, count, from, to)
+        move(acc, count, from, to, &Enum.reverse/1)
+      end
+    )
+  end
+
+  def part2() do
+    {:ok, input} = File.read("input.txt")
+
+    diagram = %{
+      "1" => ["R", "W", "F", "H", "T", "S"],
+      "2" => ["W", "Q", "D", "G", "S"],
+      "3" => ["W", "T", "B"],
+      "4" => ["J", "Z", "Q", "N", "T", "W", "R", "D"],
+      "5" => ["Z", "T", "V", "L", "G", "H", "B", "F"],
+      "6" => ["G", "S", "B", "V", "C", "T", "P", "L"],
+      "7" => ["P", "G", "W", "T", "R", "B", "Z"],
+      "8" => ["R", "J", "C", "T", "M", "G", "N"],
+      "9" => ["W", "B", "G", "L"]
+    }
+
+    [_diagram, moves] =
+      String.split(input, "\n", trim: false)
+      |> Enum.split_while(fn x -> x !== "" end)
+      |> Tuple.to_list()
+      |> Enum.map(fn x -> Enum.filter(x, fn y -> y !== "" end) end)
+
+    moves
+    |> Enum.map(fn x ->
+      Regex.scan(~r/^move (\d+) from (\d+) to (\d+)$/, x, capture: :all_but_first)
+    end)
+    |> Enum.reduce(
+      diagram,
+      fn [[count, from, to]], acc ->
+        move(acc, count, from, to, &Function.identity/1)
       end
     )
   end
