d13a2cdbf29a11f3895dfa89f4821532b9f143a6
jdl <confusionary@gmail.com>
2022-12-11 21:00:09 -0500

    attrs

--- day07/day7.ex
+++ day07/day7.ex
@@ -29,56 +29,31 @@ defmodule Day7 do
   end
 
   def add_file(filesystem, {filename, size}, []) do
-    Map.put(filesystem, filename, size)
+    Map.put(filesystem, {:f, filename}, size)
   end
 
   def add_file(filesystem, {filename, size}, path) do
     {path, cwd} = Enum.split(path, -1)
-    d = Map.get(filesystem, hd(cwd))
+    d = Map.get(filesystem, {:d, hd(cwd)})
     upd = add_file(d, {filename, size}, path)
-    Map.put(filesystem, hd(cwd), upd)
+    Map.put(filesystem, {:d, hd(cwd)}, upd)
   end
 
   def add_dir(filesystem, dir, []) do
-    Map.put(filesystem, dir, %{})
+    Map.put(filesystem, {:d, dir}, %{})
   end
 
   def add_dir(filesystem, dir, path) do
     {path, cwd} = Enum.split(path, -1)
-    d = Map.get(filesystem, hd(cwd))
+    d = Map.get(filesystem, {:d, hd(cwd)})
     upd = add_dir(d, dir, path)
-    Map.put(filesystem, hd(cwd), upd)
+    Map.put(filesystem, {:d, hd(cwd)}, upd)
   end
 
   def part1() do
     {:ok, input} = File.read("input.txt")
-    # input = """
-    # $ cd /
-    # $ ls
-    # dir a
-    # 14848514 b.txt
-    # 8504156 c.dat
-    # dir d
-    # $ cd a
-    # $ ls
-    # dir e
-    # 29116 f
-    # 2557 g
-    # 62596 h.lst
-    # $ cd e
-    # $ ls
-    # 584 i
-    # $ cd ..
-    # $ cd ..
-    # $ cd d
-    # $ ls
-    # 4060174 j
-    # 8033020 d.log
-    # 5626152 d.ext
-    # 7214296 k
-    # """
 
     [_ | terminal] = input |> String.split("\n", trim: true)
-    execute(terminal, %{"/" => %{}}, ["/"])
+    execute(terminal, %{{:d, "/"} => %{}}, ["/"])
   end
 end
